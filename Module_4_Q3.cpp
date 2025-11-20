ce4ea40bbc44c06c217aca97a6f2aa935d026784
samaksh-shrivastava <samakshshrivastava60@gmail.com>
2025-11-20 21:30:13 +0530

    Add binary tree structure and preorder function
    Implement a binary tree with preorder traversal.

---
+++ Module_4_Q3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+};
+
+Node* newNode(int value) {
+    Node *node = new Node;
+    node->data = value;
+    node->left = nullptr;
+    node->right = nullptr;
+    return node;
+}
+
+void preorder(Node *root) {
+    if (root == nullptr) return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main() {
+    Node *root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    preorder(root);
+    cout << endl;
+    return 0;
+}
