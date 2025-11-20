c688f00f4815e8107a798223db14ebfdf15f3671
samaksh-shrivastava <samakshshrivastava60@gmail.com>
2025-11-20 22:05:21 +0530

    Implement merge sort algorithm in Module_5_Q10.cpp

---
+++ Module_5_Q10.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void mergeArrays(int a[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[1000], R[1000];
+    for (int i = 0; i < n1; i++) L[i] = a[l + i];
+    for (int j = 0; j < n2; j++) R[j] = a[m + 1 + j];
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            a[k] = L[i];
+            i++;
+        } else {
+            a[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        a[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        a[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int a[], int l, int r) {
+    if (l >= r) return;
+    int m = l + (r - l) / 2;
+    mergeSort(a, l, m);
+    mergeSort(a, m + 1, r);
+    mergeArrays(a, l, m, r);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int a[1000];
+    for (int i = 0; i < n; i++) cin >> a[i];
+    mergeSort(a, 0, n - 1);
+    for (int i = 0; i < n; i++) cout << a[i] << " ";
+    cout << endl;
+    return 0;
+}
